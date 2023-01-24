9e6901ef5b7ffee7495c0b6ef5052e9a7c828631
Romain J <romain.ordi@gmail.com>
2023-01-24 12:37:01 +0100

    fix(cli): fix improper flag parsing on windows

--- utils/cli.go
+++ utils/cli.go
@@ -1,6 +1,10 @@
 package utils
 
-import "flag"
+import (
+	"flag"
+	"runtime"
+	"strings"
+)
 
 type Flags struct {
 	InputFile  string
@@ -11,12 +15,21 @@ type Flags struct {
 
 func GetFlags() Flags {
 	inputFile := flag.String("input", "", "Input file")
-	outputFile := flag.String("output", "output.csv", "Output file")
+	outputFile := flag.String("output", "", "Output file (default \"stdout\")")
 	regex := flag.String("regex", "", "Regex to use")
 
 	threads := flag.Int("threads", 12, "Number of threads to use")
 
 	flag.Parse()
+
+	if runtime.GOOS == "windows" {
+		return Flags{
+			InputFile:  strings.TrimRight(*inputFile, `'"`),
+			OutputFile: *outputFile,
+			Regex:      *regex,
+			Threads:    *threads,
+		}
+	}
 
 	return Flags{
 		InputFile:  *inputFile,
